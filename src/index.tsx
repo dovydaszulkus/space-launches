2ea73a7cf77bc6c1cba5c7915c71544001c4c89d
dovydaszulkus <dovydaszulkus@gmail.com>
2022-09-19 18:42:54 +0100

    Add route for bookmarks page

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import Header from './layout/header/Header'
+import Bookmarks from './pages/bookmarks/Bookmarks'
 import Home from './pages/home/Home'
 import UpcomingLaunches from './pages/upcomingLaunches/UpcomingLaunches'
 
@@ -14,6 +15,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/upcoming-launches" element={<UpcomingLaunches />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
